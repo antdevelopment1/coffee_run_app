196df64e20d842cbac941767984d71bd1742b724
April Copes <antdevelopment1@gmail.com>
2018-10-19 14:58:51 -0400

    Adds everyones orders to screen

--- scripts/index.js
+++ scripts/index.js
@@ -1,20 +1,25 @@
-// =======================================
+// =========
+// Constants
+// =========
+const API_URL = 'https://dc-coffeerun.herokuapp.com/api/coffeeOrders';
+
+// =============
 // Dom Selection
-// =======================================
+// =============
 const orderForm = document.querySelector('[data-form]');
 const notificationArea = document.querySelector('[data-notification]');
 
 const orderListingArea = document.querySelector('[data-order-area]');
 const orderListingButton = document.querySelector('[data-load-orders]')
 
-// =======================================
+// ================
 // Helper Functions
-// =======================================
+// ================
 function handleSubmit(event) {
     event.preventDefault();
     // console.log('You get a coffee. And you get a monkey. And you get a puppy');
 
-    const url = event.target.action;
+    const url = API_URL;
     const method = event.target.method;
     const elements =  event.target.elements;
     const data = {
@@ -59,16 +64,36 @@ function notifyUser(notificationText) {
     notificationArea.appendChild(notificationBox);
 }
 
-function getAndShowOrders(event) {
-    console.log('Hey a click');
-    fetch()
-    // console.log(event);
-
+function convertOrderToElement(orderInfo) {
+    // debugger;
+    const orderElement = document.createElement('p');
+    const orderText = `${orderInfo.size} ${orderInfo.flavor} ${orderInfo.coffee} for ${orderInfo.emailAddress} ${orderInfo.strength}`;
+    orderElement.innerHTML = orderText;
+    return orderElement;
 }
 
-// =======================================
+// Working with an array of coffee orders
+function convertArrayOfOrdersToElements(giantOrder) {
+    let orderArray = Object.values(giantOrder);
+    let elementsArray = orderArray.map(convertOrderToElement);
+    return elementsArray;
+}
+
+function getAndShowOrders(event) {
+    console.log('Hey a click');
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(convertArrayOfOrdersToElements)
+        .then(elementsArray => {
+            // debugger;
+            elementsArray.forEach(e => orderListingArea.appendChild(e));
+        })
+    // console.log(event);
+}
+
+// ====================
 // Main Event Listeners
-// =======================================
+// ====================
 console.log('About to add event listener');
 orderForm.addEventListener('submit', handleSubmit);
 orderListingButton.addEventListener('click', getAndShowOrders);
